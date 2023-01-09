f48273c51b614e2cd138e7352d6c7d4ed071c473
tung <caothanhtung08082002@gmail.com>
2023-01-09 20:57:55 +0700

    my project

--- asd/src/module-info.java
+++
@@ -1,5 +0,0 @@
-module asd {
-	requires javafx.controls;
-	
-	opens application to javafx.graphics, javafx.fxml;
-}
--- dfg/src/module-info.java
+++
@@ -1,5 +0,0 @@
-module dfg {
-	requires javafx.controls;
-	
-	opens application to javafx.graphics, javafx.fxml;
-}
